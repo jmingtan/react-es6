ab6b0791e1d190600fc9be39a303657e0099e133
Eugene Tan <jmingtan@gmail.com>
2015-05-06 21:43:40 +0800

    Retrieve comments.json from server

--- src/comments/CommentBox.js
+++ src/comments/CommentBox.js
@@ -1,13 +1,30 @@
 const React = require('React');
+const $ = require('jquery');
 const CommentList = require('./CommentList');
 const CommentForm = require('./CommentForm');
 
 const CommentBox = React.createClass({
+    getInitialState() {
+        return {data: []};
+    },
+    componentDidMount() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: (data) => {
+                this.setState({data: data});
+            },
+            error: (xhr, status, err) => {
+                console.error(this.props.url, status, err.toString());
+            }
+        });
+    },
     render() {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data} />
+                <CommentList data={this.state.data}/>
                 <CommentForm />
             </div>
         )
--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 var React = require('React');
 var CommentBox = require('./comments/CommentBox');
 
-const data = [
-    {author: "Pete Hunt", text: "This is one comment"},
-    {author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 React.render(
-    <CommentBox data={data} />,
+    <CommentBox url="comments.json" />,
     document.getElementById('app')
 );
