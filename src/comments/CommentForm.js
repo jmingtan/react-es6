3ccd5e4e218b4b1d8e1efc289eaa151cc4e8585d
Eugene Tan <jmingtan@gmail.com>
2015-05-06 21:50:45 +0800

    Construct comment form

--- src/comments/CommentForm.js
+++ src/comments/CommentForm.js
@@ -1,11 +1,25 @@
 const React = require('React');
 
 const CommentForm = React.createClass({
+    handleSubmit(e) {
+        e.preventDefault();
+        const author = React.findDOMNode(this.refs.author).value.trim();
+        const text = React.findDOMNode(this.refs.text).value.trim();
+        if (!text || !author) {
+            return;
+        }
+        // TODO: send request to the server
+        React.findDOMNode(this.refs.author).value = '';
+        React.findDOMNode(this.refs.text).value = '';
+        return;
+    },
     render() {
         return (
-            <div className="commentForm">
-                Hello, world! I am a CommentForm.
-            </div>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" ref="author" />
+                <input type="text" placeholder="Say something..." ref="text" />
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 });
